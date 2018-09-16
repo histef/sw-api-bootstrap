33dba67c4bfdb7fc33556718df1ee0f9c72c1ffb
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-09-16 16:35:39 -0700

    fix: minor fixes to layout, content, styling

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 
-// const cards = document.querySelectorAll('.card');
-let createCard;
-
 
 for (let i=0;i<3;i++){
+  const addCardBody = document.querySelector('.cards');
 
-  let randomPerson = Math.floor(Math.random()*(Math.floor(87)-Math.ceil(0)));
+  let randomPerson = Math.floor((Math.random()*87)+1);
   //fetch Star Wars API
   fetch(`https://swapi.co/api/people/${randomPerson}`)
     .then( response => {
@@ -13,9 +11,9 @@ for (let i=0;i<3;i++){
       return response.json();
     })
     .then(data => {
-      console.log(data)
+      // console.log(data)
 
-      createCard = `<div class="col-md-4">
+      let createCard = `<div class="col-md-4">
           <div class="card">
             <div class="card-body">
               <h4 class="card-dash-title">${data.name}</h4>
@@ -27,25 +25,15 @@ for (let i=0;i<3;i++){
           </div>
         </div>
   `
-  const addCardBody = document.querySelector('.cards');
+
   addCardBody.innerHTML += createCard;
   })
   .catch( error => console.log(`ERROR: ${error}`))
 }
 
+
+
 // NOTE: star wars person 17 is 'not found', 404 error
 
-
-//populate card-dash-title card-text)
-
-   // <div class=" cards row padding">
-   //    <div class="col-md-4">
-   //      <div class="card">
-   //        <!-- <img src="#" alt="star wars" class="car-img-top"> -->
-   //        <div class="card-body">
-   //          <h4 class="card-dash-title">Luke</h4>
-   //          <p class="card-text">Luke Skywalker, last jedi</p>
-   //          <a href="#" class="btn btn-outline-secondary">See Profile</a>
-   //        </div>
-   //      </div>
-   //    </div>
+// TODO: Handle no duplicates
+// TODO: Handle #17 (if no info exists)
