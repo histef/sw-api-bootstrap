1c3f7e871525dbac4f6c8ed4813616decb4a0b85
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-09-15 20:34:15 -0700

    feat: add dynamically rendering character cards with data from SWAPI

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const card1 = document.querySelector('.cards').firstChild;
-const card2 = document.querySelector('.cards').nextSibling;
-const card3 = document.querySelector('.cards')[2];
 
-const cards = document.querySelector('.card-body')
+// const cards = document.querySelectorAll('.card');
+let createCard;
 
 
-const randomPerson = Math.floor(Math.random()*(Math.floor(87)-Math.ceil(0)));
-console.log(randomPerson)
+for (let i=0;i<3;i++){
 
-// //fetch Star Wars API
-// fetch(`https://swapi.co/api/people/${randomPerson}`)
-//   .then( response=>{
-//     //return response object
-//     return response.json();
-//   })
-//   .then(logJSON=>{
-//     console.log(logJSON);
-//   })
-//   .catch( error => console.log(`ERROR: ${error}`))
+  let randomPerson = Math.floor(Math.random()*(Math.floor(87)-Math.ceil(0)));
+  //fetch Star Wars API
+  fetch(`https://swapi.co/api/people/${randomPerson}`)
+    .then( response => {
+      //return response object
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+
+      createCard = `<div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-dash-title">${data.name}</h4>
+              <p class="card-text">Birth Year: ${data.birth_year}</p>
+              <p class="card-text">Gender: ${data.gender}</p>
+              <p class="card-text">Height: ${data.height}</p>
+              <a href="#" class="btn btn-outline-secondary">See Profile</a>
+            </div>
+          </div>
+        </div>
+  `
+  const addCardBody = document.querySelector('.cards');
+  addCardBody.innerHTML += createCard;
+  })
+  .catch( error => console.log(`ERROR: ${error}`))
+}
+
+// NOTE: star wars person 17 is 'not found', 404 error
 
 
-//cards.map(get a random character(randomPerson) then fetch the data for them
 //populate card-dash-title card-text)
 
    // <div class=" cards row padding">
