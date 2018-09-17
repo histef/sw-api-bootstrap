77a1cda8d000b8f41749acae00b2459b62259d25
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-09-16 20:21:55 -0700

    feat: randomized quote shuffler

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ for (let i=0;i<3;i++){
   .catch( error => console.log(`ERROR: ${error}`))
 }
 
+const quotes = [
+  {quote: "Remember: Your focus determines your reality.", speaker: 'Qui-Gon Jinn'},
+  {quote: 'You can’t stop change any more than you can stop the suns from setting.', speaker: 'Shmi Skywalker'},
+  {quote: 'What if the democracy we thought we were serving no longer exists, and the Republic has become the very evil we have been fighting to destroy?', speaker: 'Padmé'},
+  {quote: 'Attachment is forbidden. Possession is forbidden. Compassion, which I would define as unconditional love, is essential to a Jedi’s life. So you might say, that we are encouraged to love.', speaker: 'Anakin Skywalker'},
+  {quote: 'Fear is the path to the dark side.', speaker: 'Yoda'}
+  ]
+
+function getQuote(){
+  const jumbotron = document.querySelector('.lead');
+  const quoteCredit = document.querySelector('.quote-credit');
+  let randomIndex = Math.floor((Math.random()*5));
+
+  jumbotron.textContent = `${quotes[randomIndex].quote}`;
+  quoteCredit.textContent =  `${quotes[randomIndex].speaker}`;
+
+}
+
+const quoteBtn = document.querySelector('.btn-outline-secondary');
+quoteBtn.addEventListener('click', getQuote);
 
 
 // NOTE: star wars person 17 is 'not found', 404 error
